b4688a80138faa6e21bd0b44da0b1984d949a2a6
Victor Noventa <victor.noventa@icloud.com>
2017-11-30 11:19:23 -0200

    Portuguese adapted classifiers

---
+++ src/portuguese/ngrams/naivebayes/GaussianNB.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from random import shuffle
+import nb_helper
+
+import sys
+sys.path.append('data/portuguese/')
+from dataset import Dataset
+
+from sklearn.naive_bayes import GaussianNB
+
+SUBSETS_COUNT = 50
+TRAIN_SUBSETS_COUNT = int(0.8 * SUBSETS_COUNT)
+TEST_SUBSETS_COUNT = SUBSETS_COUNT - TRAIN_SUBSETS_COUNT
+
+d = Dataset()
+custom_features = d.custom_features(d.full_set, nb_helper.get_trigrams)
+
+corpus_features = set([feature for phrase_features in custom_features.values() for feature in phrase_features])
+generated_subsets = [subset for subset in d.get_subsets(SUBSETS_COUNT)]
+shuffle(generated_subsets) # randomize subsets
+
+train_set = {phrase for subset in generated_subsets[:TRAIN_SUBSETS_COUNT] for phrase in subset.items()}
+X = []
+y = []
+for phrase, intent in train_set:
+    X.append(nb_helper.format_features_for_classifier(custom_features[phrase], corpus_features))
+    y.append(intent)
+
+classifier = GaussianNB()
+classifier.fit(X, y)
+
+test_set = {phrase for subset in generated_subsets[-TEST_SUBSETS_COUNT:] for phrase in subset.items()}
+accepted = 0
+for phrase, intent in test_set:
+    predicted = classifier.predict([nb_helper.format_features_for_classifier(custom_features[phrase], corpus_features)])
+    if predicted[0] == intent:
+        accepted += 1
+    else:
+        print(phrase, ' [predicted: ', predicted[0], '; correct: ', intent, ']')
+
+total = len(test_set)
+print('SCORE: ', accepted, '/', total, ' (', accepted/(1.0 * total), ')')
---
+++ src/portuguese/vectors/neuralnetwork/nn_helper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+W2V_SIZE = 25
+
+def format_features_for_classifier(phrase_features, vectors):
+    features = np.zeros(W2V_SIZE)
+    for feature in phrase_features:
+        features = np.add(features, vectors[feature])
+    return features
+
+def get_tokens(phrase):
+    return phrase.split()
