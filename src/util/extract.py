0ef1b59d647fdcb91caa4f18f0824e4f7325b2cb
Victor Noventa <victor.noventa@icloud.com>
2017-12-02 21:00:36 -0200

    Added confusion matrix figures

--- src/util/extract.py
+++ src/util/extract.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, re, ast
+import sys, re, ast, itertools
 
 file_absolute_path = sys.argv[1]
 intents_list = ast.literal_eval(sys.argv[2])
+figure_title = sys.argv[3]
+figure_cmap = sys.argv[4]
 
 wrong_phrases_classifications = []
 scores = []
@@ -25,10 +27,9 @@ for classified_phrase in wrong_phrases_classifications:
         correct_classifications.append(data[1][4])
 
 from sklearn.metrics import confusion_matrix
+confusion_matrix_data = confusion_matrix(correct_classifications, predicted_classifications, labels=intents_list)
 
-print("EXTRACTED INFO")
-print("\nconfusion_matrix")
-print(confusion_matrix(correct_classifications, predicted_classifications, labels=intents_list))
+import numpy as np
 
 accepted_scores = []
 total_scores = []
@@ -37,17 +38,55 @@ for score in scores:
     accepted_scores.append(data[1])
     total_scores.append(data[3])
 
-import numpy as np
-
 a = np.array(accepted_scores, dtype=np.float)
 b = np.array(total_scores, dtype=np.float)
-
 means = a / b
-print("\nmeans")
-print(means)
 
-print("\naverage")
-print(np.average(means))
+file_absolute_path_without_extension = file_absolute_path.split('.')[0]
+extracted_file = open(file_absolute_path_without_extension + '_extracted.txt', 'w')
 
-print("\nstd")
-print(np.std(means))
+extracted_file.write("EXTRACTED INFO")
+extracted_file.write("\n\nconfusion_matrix\n")
+extracted_file.write(np.array_str(confusion_matrix_data))
+
+extracted_file.write("\n\nmeans\n")
+extracted_file.write(np.array_str(means))
+
+extracted_file.write("\n\naverage\n")
+extracted_file.write(np.array_str(np.average(means)))
+
+extracted_file.write("\n\nstd\n")
+extracted_file.write(np.array_str(np.std(means)))
+
+extracted_file.close()
+
+import matplotlib.pyplot as plt
+np.set_printoptions(precision=2)
+
+def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues):
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Plot non-normalized confusion matrix
+fig = plt.figure(figsize=[12, 10])
+plot_confusion_matrix(confusion_matrix_data, title=figure_title, classes=intents_list, cmap=figure_cmap)
+
+fig.tight_layout()
+plt.savefig(file_absolute_path_without_extension + '_confusion_matrix.png', format='png', dpi=300)
